e90a504d389601c606d8e46d506c3517e4ff8005
Donato Laynes <donato.laynes@senial.net>
2016-07-05 22:20:58 -0500

    Ship Selector - base

--- src/AppState.js
+++ src/AppState.js
@@ -1,27 +1,27 @@
 import { observable } from 'mobx';
 
 class AppState {
-  @observable gameState = 1;
+    @observable gameState = 1;
 
-  gameStates = {
-      home : 1,
-      ships : 2,
-      space: 3,
-      event : 4,
-      battleResult : 5,
-      planet: 6,
-      endBad: 7,
-      endGood: 8
-  };
+    gameStates = {
+        home : 1,
+        ships : 2,
+        space: 3,
+        event : 4,
+        battleResult : 5,
+        planet: 6,
+        endBad: 7,
+        endGood: 8
+    };
 
-  constructor() {
+    constructor() {
 
-  }
+    }
 
-  changeState(state){
-      this.gameState = state;
-      console.log(this.gameState);
-  }
+    /* We trust you*/
+    changeState(state){
+        this.gameState = state;
+    }
 }
 
 export default AppState;
