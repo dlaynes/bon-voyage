970552ce3e1c801e4dfc8c3e4d9c4a38cdbba2ee
Donato Laynes <donato.laynes@senial.net>
2016-07-13 19:11:47 -0500

    Production

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -8,14 +8,31 @@ class HomeComponent extends Component {
     render () {
         return (
             <div className={ this.props.visibility ? '' : 'hidden' }>
-                <h3>Welcome</h3>
-                <p>Good morning Admiral.</p>
-                <p>Blah blah blah lorem Ipsum</p>
+                <h3>Bon Voyage</h3>
+                <p><span className="text-warning">The sector is at war. Greed and corruption splat the Confederation in half.<br />
+                    Outlaws attack everybody on sight. For many of them there is a price on their heads.
+                    <br />Two enemy civilizations, <span className="text-info">The Scourge</span> and <span className="text-info">Quadrant 12</span>
+                    , threaten to destroy what is left of the old Government...</span></p>
+                <br /><br />
+                <p><span className="text-success">
+                    Good morning Admiral. Your mission is to reach planet Omega, in order to deliver confidential documents to the Confederates.<br />
+                    You are free to build your own ships. We really trust your instincts, so we will give you a part of our resources to do so.
+                    </span>
+                </p>
+                <br />
+                <p><span className="text-success">
+                    There are two ally planets on the way.<br />
+                    But you will need Credits in order to upgrade the fleet, and buy more fuel.<br /><br />Good luck.
+                    </span>
+                </p>
+                <br />
                 <div className="clear"></div>
                 <div className="text-center">
                     <button onClick={(e) => this.goToShipSelection(e)}
                             className="action-red">» CREATE EXPEDITION FLEET</button>
                 </div>
+
+                <small className="pull-right"><br />Bon Voyage v.0.2</small>
             </div>
         );
     }
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+//import { AppContainer } from 'react-hot-loader';
 import AppStore from './Stores/AppStore';
 import App from './App';
 
 const appStore = new AppStore();
-appStore.debugMode = true;
+appStore.debugMode = false;
 
 render(
-  <AppContainer>
-    <App appStore={appStore} />
-  </AppContainer>,
-  document.getElementById('root')
+    <App appStore={appStore} />,
+    document.getElementById('root')
 );
 
+/*
 if (module.hot) {
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default;
@@ -26,3 +25,4 @@ if (module.hot) {
     );
   });
 }
+*/
